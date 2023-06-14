c8a818f927603e0dd83ccc832b5265a0a5f3efe1
lingi524 <linnea.gistrand@gmail.com>
2023-06-14 16:09:46 +0200

    Add animation to page

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,12 +1,30 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Paper = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const variants = {
+    clicked: { height: 840, transition: { duration: 0.5 } },
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <motion.div className="bg-[#F0E8D8] h-[430px] w-[922px] flex flex-col items-start pt-8 pl-12">
+      <motion.div
+        className="bg-[#F0E8D8] h-[430px] w-[922px] flex flex-col items-start pt-8 pl-12"
+        variants={variants}
+        animate={isClicked ? "clicked" : ""}
+      >
         <div>
-          <p className="text-xl">
-            Hej <strong>Alex,</strong>
+          <p className={"text-xl " + (isClicked ? "text-black/30	" : "")}>
+            Hej{" "}
+            <strong
+              onClick={() => {
+                setIsClicked(true);
+              }}
+            >
+              Alex,
+            </strong>
           </p>
         </div>
       </motion.div>
